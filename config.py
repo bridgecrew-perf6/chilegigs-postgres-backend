9d5339e060cd38ad5239f7143c61dba416aea638
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-12-28 00:27:43 -0300

    testing 4

--- config.py
+++ config.py
@@ -11,9 +11,13 @@ class Base:
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
     SECRET_KEY = ''
     MAIL_SERVER  = environ.get('MAIL_SERVER')
-    MAIL_PORT = environ.get('MAIL_PORT')
-    MAIL_USE_TLS = bool(environ.get('MAIL_USE_TLS'))
-    MAIL_DEBUG  = bool(environ.get('MAIL_DEBUG'))
+    MAIL_PORT = 587
+    # MAIL_USE_TLS = bool(environ.get('MAIL_USE_TLS'))
+    # MAIL_DEBUG  = bool(environ.get('MAIL_DEBUG'))
+    MAIL_USE_TLS = True
+    MAIL_DEBUG  = True
+    TESTING = False
+    MAIL_SUPPRESS_SEND = False
     MAIL_USERNAME = environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER  = environ.get('MAIL_USERNAME')
